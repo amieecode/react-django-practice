dfa49c82d5f71661a7f0b26f4f8e48fc0b7d59e9
amieecode <enwelumchiamaka@gmail.com>
2025-03-07 01:25:29 +0100

    react set up

---
+++ react-basic-form/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import BasicForm from './Components/BasicForm'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Basic Form</h1>
+      <BasicForm />
+    </>
+  )
+}
+
+export default App
