aaf29bb557cfe2ec1492596f7cf320813090804d
amieecode <enwelumchiamaka@gmail.com>
2025-03-08 21:20:44 +0100

    form

--- react-basic-form/src/Components/BasicForm.jsx
+++ react-basic-form/src/Components/BasicForm.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const BasicForm = () => {
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log('form Submitted')
+    }
+
   return (
     <div>
-      
+      <form onClick={handleSubmit}>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
