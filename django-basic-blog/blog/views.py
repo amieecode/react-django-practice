68853cc9c46b1f732f49881ea58f4e4af371b325
amieecode <enwelumchiamaka@gmail.com>
2025-03-08 23:59:26 +0100

    Authentication

--- django-basic-blog/blog/views.py
+++ django-basic-blog/blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Post
 
 # Create your views here.
 def home(request):
     posts = Post.objects.all()
     return render(request, 'blog/home.html', {'posts': posts})
+
+def user_login(request):
+    if request.method = 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+        else:
+            form = AuthenticationForm()
+
+        return render(request, blog/login.html, {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
