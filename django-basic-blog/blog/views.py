59fafbc074439f03ae770eaa52d96f01428e3f88
amieecode <enwelumchiamaka@gmail.com>
2025-03-09 22:47:36 +0100

    blog system

--- django-basic-blog/blog/views.py
+++ django-basic-blog/blog/views.py
@@ -9,16 +9,16 @@ def home(request):
     return render(request, 'blog/home.html', {'posts': posts})
 
 def user_login(request):
-    if request.method = 'POST':
+    if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('home')
-        else:
-            form = AuthenticationForm()
+    else:
+        form = AuthenticationForm()
 
-        return render(request, blog/login.html, {'form': form})
+    return render(request, 'blog/login.html', {'form': form})
 
 def user_logout(request):
     logout(request)
