ba978ed91b6fddd02e96d47b4cd081aa352aeb37
amieecode <enwelumchiamaka@gmail.com>
2025-03-18 23:50:23 +0100

    logou, password rest, profile and profile updated included

--- simple-django-auth/users/urls.py
+++ simple-django-auth/users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import register_user, login_user
+from .views import ( 
+    register_user, login_user, logout_user, 
+    password_reset_request, get_user_profile, update_user_profile
+)
 
 urlpatterns = [
-    path('register/', register_user, name='register'),
-    path('login/', login_user, name='login'),
+    path('register/', register_user, name='register_user'),
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout_user'),
+    path('password-reset/', password_reset_request, name='password_reset'),
+    path('profile/', get_user_profile, name='user_profile'),
+    path('profile/update/', update_user_profile, name='update_user_profile'),
 ]
--- simple-django-auth/users/views.py
+++ simple-django-auth/users/views.py
@@ -1,9 +1,15 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from .serializers import UseSerializer
+from django.contrib.auth import authenticate, logout
+from django.core.mail import send_mail
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.urls import reverse
+from .serializers import UserSerializer
 
 # Create your views here.
 @api_view(['POST'])
@@ -15,6 +21,7 @@ def register_user(request):
         return Response({"token": token.key, "user": serializer.data})
     return Response(serializer.errors, status=400) 
 
+
 @api_view(['POST'])
 def login_user(request):
     username = request.data.get('username')
@@ -24,3 +31,68 @@ def login_user(request):
         token, _ = Token.objects.get_or_create(user=user)
         return Response({"token": token.key})
     return Response({"error": "Invalid credentials"}, status=400)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout_user(request):
+    """Logs out the user by deleting their authentication token"""
+    token = request.user.auth_token
+    if token:
+        token.delete()
+    return Response({"message": "User logged out"}, status=200)
+
+
+@api_view(['POST'])
+def password_reset_request(request):
+    """send Password reset email"""
+    email = request.data.get('email')
+    if not email:
+        return Response({"error": 'Email is required'}, status=400)
+
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response({"error": "User does not exit"}, status=400)
+    
+    token = default_token_generator.make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    reset_link = request.build_absolute_uri(reverse("password_reset_confirm", args=[uid, token]))
+
+    send_mail(
+        "Password reset request",
+        f"Click the link to reset your password:\n{reset_link}",
+        "your_email@gmail.com",
+        [email],
+        fail_silently=False,
+    )
+    return Response({"message": "Password reset email sent"}, status=200)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    """Get logged in user's profile"""
+    user = request.user
+    data = {
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+    }
+    return Response(data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_user_profile(request):
+    """"Update logged in user's profile"""
+    user = request.user
+    serializer = UserSerializer(user, data=request.data, partial=True)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=200)
+
+    return Response(serializer.errors, status=400)
